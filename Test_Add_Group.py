1ffcc6a4754094f91c937fdbf440aa42421600a0
Alexey <craftynakedman@gmail.com>
2022-10-17 15:46:24 +0300

    +

--- Test_Add_Group.py
+++ Test_Add_Group.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-
-driver = webdriver.Chrome('C:\\Windows\System32\chromedriver.exe')
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
@@ -12,62 +10,47 @@ import unittest, time, re
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
-        self.wd.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+        self.dw = webdriver.Firefox()
+        self.dw.implicitly_wait(30)
 
     def test_untitled_test_case(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("kek2")
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("kek2")
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("kek2")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("Logout").click()
+        dw = self.dw
+        dw.get("http://localhost/addressbook/group.php")
+        dw.find_element_by_name("user").clear()
+        dw.find_element_by_name("user").send_keys("admin")
+        dw.find_element_by_name("pass").click()
+        dw.find_element_by_name("pass").clear()
+        dw.find_element_by_name("pass").send_keys("secret")
+        dw.find_element_by_xpath("//input[@value='Login']").click()
+        dw.find_element_by_name("new").click()
+        dw.find_element_by_name("group_name").click()
+        dw.find_element_by_name("group_name").clear()
+        dw.find_element_by_name("group_name").send_keys("kek3")
+        dw.find_element_by_name("group_header").clear()
+        dw.find_element_by_name("group_header").send_keys("kek3")
+        dw.find_element_by_name("group_footer").clear()
+        dw.find_element_by_name("group_footer").send_keys("kek3")
+        dw.find_element_by_name("submit").click()
+        dw.find_element_by_link_text("groups").click()
+        dw.find_element_by_link_text("Logout").click()
 
     def is_element_present(self, how, what):
         try:
-            self.wd.find_element(by=how, value=what)
+            self.dw.find_element(by=how, value=what)
         except NoSuchElementException as e:
             return False
         return True
 
     def is_alert_present(self):
         try:
-            self.wd.switch_to_alert()
+            self.dw.switch_to_alert()
         except NoAlertPresentException as e:
             return False
         return True
 
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
 
     def tearDown(self):
-        self.wd.quit()
-        self.assertEqual([], self.verificationErrors)
+        self.dw.quit()
 
 
 if __name__ == "__main__":
